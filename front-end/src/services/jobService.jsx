9dafd42d734ef62192df456bef7255fa5fa0bf32
Kindeo0511 <alexprada782@gmail.com>
2026-07-28 18:49:20 +0800

    updated

--- front-end/src/services/jobService.jsx
+++ front-end/src/services/jobService.jsx
@@ -1,6 +1,5 @@
 import { authFetch } from "./authService";
-
-const API_URL = "http://127.0.0.1:8000/";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function ViewAllJobs(job_title, page) {
   const params = new URLSearchParams();
