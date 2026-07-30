a5778f8abca921cd5c6b3e92262bf20e0c8bb957
Kindeo0511 <alexprada782@gmail.com>
2026-07-30 19:29:12 +0800

    Update static files config and gitignore

--- back-end/job_admin/settings.py
+++ back-end/job_admin/settings.py
@@ -151,7 +151,6 @@ USE_TZ = True
 
 MEDIA_URL = "/media/"
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 WHITENOISE_MANIFEST_STRICT = False
