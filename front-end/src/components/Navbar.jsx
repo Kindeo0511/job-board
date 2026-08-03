20f2b57bd221b98fa26dbd0736f428819f5e4961
Kindeo0511 <alexprada782@gmail.com>
2026-08-03 15:24:00 +0800

    updated nav link

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -77,27 +77,31 @@ function Navbar({ isLoggedIn }) {
                 />{" "}
               </svg>
             </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+            {isLoggedIn && (
+              <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li>
+                  <Link to={role === "JS" ? "/" : "/employer/homepage"}>
+                    {role === "JS" ? "Browse Job" : "My Job Posts"}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={role === "JS" ? "My/Applications" : "/MyApplicants"}>
+                    {role === "JS" ? "My Applications" : "My Applicants"}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={
+                      role === "JS" ? "/profile/jobseeker" : "/profile/employer"
+                    }>
+                    Profile
+                  </Link>
+                </li>
+              </ul>
+            )}
           </div>
           {isLoggedIn ? (
             <Link to={role === "JS" ? "/" : "/employer/homepage"}>Jobless</Link>
